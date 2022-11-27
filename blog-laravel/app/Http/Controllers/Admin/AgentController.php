3754e8be3321c1d1d37f56172e2c4185c93a179e
DmytroRadulov <radulovdima1995@gmail.com>
2022-11-27 12:31:46 +0200

    dz_20

--- blog-laravel/app/Http/Controllers/Admin/AgentController.php
+++ blog-laravel/app/Http/Controllers/Admin/AgentController.php
@@ -3,27 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\TestAgent;
 use Package\Interface\Test\UserAgentInterface;
-use App\Models\Visit;
 
 class AgentController extends Controller
 {
     public function index(UserAgentInterface $userAgent)
     {
-        $ip = request()->ip();
-        if ($ip == '127.0.0.1') {
-            $ip = request()->server->get('HTTP_X_FORWARDED_FOR');
-        }
-
-        $browser = $userAgent->getBrowser();
-        $platform = $userAgent->getPlatform();
-
-        if (!empty($browser) && !empty($platform)) {
-            Visit::create([
-                'browser' => $browser,
-                'platform' => $platform,
-            ]);
-        }
+        TestAgent::dispatch($userAgent)->onQueue('parsing');
     }
 }
 
--- blog-laravel/database/seeders/DatabaseSeeder.php
+++ blog-laravel/database/seeders/DatabaseSeeder.php
@@ -2,14 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Jobs\TestAgent;
+use App\Models\Job;
 use App\Models\Page;
 use App\Models\Post;
 use App\Models\Review;
 use App\Models\Tag;
 use App\Models\Category;
 use App\Models\User;
+use Faker\Provider\UserAgent;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Facade;
+use Jenssegers\Agent\Agent;
+use Package\UserAgent\Test\ParserJenssegers;
 
 class DatabaseSeeder extends Seeder
 {
@@ -33,5 +38,11 @@ class DatabaseSeeder extends Seeder
 
         $pages = Page::factory(20)->create();
         $reviews = Review::factory(20)->create();
+
+        $userAgent = new ParserJenssegers();
+        for ($i = 0; $i < 20; $i++) {
+            $userAgent->setUserAgent(fake()->userAgent());
+            TestAgent::dispatch($userAgent)->onQueue('parsing');
+        }
     }
 }
